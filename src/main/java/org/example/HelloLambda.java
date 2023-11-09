4985ecf0c34747401fb01a43293c7c1b71b6c8ed
Jur Tasseron <JAJ.Tasseron@student.han.nl>
2023-11-09 11:01:58 +0100

    Code

--- src/main/java/org/example/HelloLambda.java
+++ src/main/java/org/example/HelloLambda.java
@@ -1,2 +1,8 @@
-package org.example;public class HelloLambda {
+package org.example;
+
+public class HelloLambda {
+
+    public String handleRequest(String name){
+        return "Hello, "+name+"!";
+    }
 }
--- src/test/java/org/example/HelloLambdaTest.java
+++ src/test/java/org/example/HelloLambdaTest.java
@@ -1,4 +1,14 @@
-import junit.framework.TestCase;
-public class HelloLambdaTest extends TestCase {
-  
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class HelloLambdaTest {
+
+    @Test
+    public void shouldReturnHelloJurForParamJur() {
+        var sut = new HelloLambda();
+        assertEquals("Hello, Jur!", sut.handleRequest("Jur"));
+    }
 }
